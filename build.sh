fa1c71ed9cdaa4cfa6b0de00b55fa81aaea6267e
Samuel Venable <samuelvenable@hotmail.com>
2022-05-10 20:19:34 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 cd "${0%/*}"
 
 if [ $(uname) = "Darwin" ]; then
-  clang++ -c gamemaker.cpp apiprocess/process.cpp -shared -std=c++17 -DPROCESS_GUIWINDOW_IMPL -framework Cocoa -framework CoreFoundation -framework CoreGraphics -ObjC++ -arch arm64 -arch x86_64 && clang++ gamemaker.o process.o -o libxprocess.dylib -shared -std=c++17 -DPROCESS_GUIWINDOW_IMPL -framework CoreFoundation -framework CoreGraphics -framework Cocoa -ObjC++ -arch arm64 -arch x86_64;
+  clang++ -c gamemaker.cpp apiprocess/process.cpp -shared -std=c++17 -Wno-unused-command-line-argument -DPROCESS_GUIWINDOW_IMPL -framework Cocoa -framework CoreFoundation -framework CoreGraphics -ObjC++ -arch arm64 -arch x86_64 && clang++ gamemaker.o process.o -o libxprocess.dylib -shared -std=c++17 -Wno-unused-command-line-argument -DPROCESS_GUIWINDOW_IMPL -framework CoreFoundation -framework CoreGraphics -framework Cocoa -ObjC++ -arch arm64 -arch x86_64;
   ar rc libxprocess.a gamemaker.o process.o && rm -f "gamemaker.o" "cocoa.o" "process.o";
 elif [ $(uname) = "Linux" ]; then
   g++ -c gamemaker.cpp apiprocess/process.cpp -shared -std=c++17 -static-libgcc -static-libstdc++ -lprocps -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -fPIC && g++ gamemaker.o process.o -o  libxprocess.so -shared -std=c++17 -static-libgcc -static-libstdc++ -lprocps -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -fPIC;
