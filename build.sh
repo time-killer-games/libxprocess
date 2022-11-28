eb9fdad07a5481c7b795bbdb19fab3ca81e4660b
Samuel Venable <samuelvenable@hotmail.com>
2022-11-28 02:25:55 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ elif [ $(uname) = "OpenBSD" ]; then
   clang++ -c gamemaker.cpp lib/cproc/cproc.cpp lib/xproc/xproc.cpp -I. -shared -std=c++17 -I/usr/local/include -L/usr/local/lib -lkvm -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -fPIC && clang++ gamemaker.o cproc.o xproc.o -o libxprocess.so -I. -shared -std=c++17 -I/usr/local/include -L/usr/local/lib -lkvm -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -fPIC;
   ar rc libxprocess.a gamemaker.o cproc.o xproc.o && rm -f "gamemaker.o" "cproc.o" "xproc.o";
 elif [ $(uname) = "SunOS" ]; then
-  g++ -c gamemaker.cpp lib/cproc/cproc.cpp lib/xproc/xproc.cpp -I. -shared -std=c++17 -static-libgcc -static-libstdc++ -lkvm -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -fPIC && g++ gamemaker.o cproc.o xproc.o -o libxprocess.so -I. -shared -std=c++17 -lkvm -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -fPIC;
+  g++ -c gamemaker.cpp lib/cproc/cproc.cpp lib/xproc/xproc.cpp -I. -shared -std=c++17 -static-libgcc -lkvm -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -fPIC && g++ gamemaker.o cproc.o xproc.o -o libxprocess.so -I. -shared -std=c++17 -lkvm -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -fPIC;
   ar rc libxprocess.a gamemaker.o cproc.o xproc.o && rm -f "gamemaker.o" "cproc.o" "xproc.o";
 else
   g++ -c gamemaker.cpp lib/cproc/cproc.cpp lib/xproc/xproc.cpp -I. -shared -std=c++17 -static-libgcc -static-libstdc++ -static -DPROCESS_GUIWINDOW_IMPL && g++ gamemaker.o cproc.o xproc.o -o  libxprocess.dll -I. -shared -std=c++17 -static-libgcc -static-libstdc++ -static -DPROCESS_WIN32EXE_INCLUDES -DPROCESS_GUIWINDOW_IMPL;
